4513942bb5476fb4b545be31b3490329b6e7720d
YuYang-Dev <79308509+YuYang-Dev@users.noreply.github.com>
2022-10-28 21:27:08 +0800

    Add files via upload
    haha yes

---
+++ main.js
@@ -0,0 +1,37 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftwristx = " + leftWristX + "+201x3^4 " + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightwristx = " + rightWristX + "+201x3^4 " + "rightWristY = " + rightWristY);
+    }
+}
